95b3cd079d84618b4e8886295a7c2957a9bf8301
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-07-17 21:08:12 -0400

    Completed step 1 and 2 in prototypes.js

--- challenges/closure.js
+++ challenges/closure.js
@@ -22,3 +22,15 @@ myFunction();
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+const summation = (num) => {
+  let sum = 1;
+  let sum2 = () => {
+    for(i = 1; i < (num + 1); i++){
+      sum = sum + i;
+      return sum;
+    };
+  };
+};
+
+console.log(summation(4));
