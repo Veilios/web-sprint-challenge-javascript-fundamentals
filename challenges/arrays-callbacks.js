c36ad01f433a19fd62b623f103ef14c659481a55
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-07-17 20:39:11 -0400

    Completed Step 1 inside array-callbacks.js

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -61,12 +61,17 @@ let populationTotal = zooAnimals.reduce(sum, 0);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+let consume = (a, b, cb) => {
+  return cb(a, b);
+};
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
